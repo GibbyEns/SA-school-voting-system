62a6dbbf0dc04a9bc987565f79dce602f31491e1
Nongcebo Mnikati <gibbymerc@gmail.com>
2025-11-29 13:45:41 +0200

    update

--- votes/urls.py
+++ votes/urls.py
@@ -4,10 +4,8 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    path('', views.welcome, name='welcome'),
-    path('survey/<int:poll_id>/', views.survey, name='survey'),
-    path('vote/', views.vote, name='vote'),  # <-- fixed
-    path('results/<int:poll_id>/', views.results, name='results'),
-    path('thankyou/', views.thank_you, name='thank_you'),
+    path('vote/<int:poll_id>/', views.vote, name='vote'),
+path('survey/<int:poll_id>/', views.survey, name='survey'),
+path('results/<int:poll_id>/', views.results, name='results'),
 ]
 
